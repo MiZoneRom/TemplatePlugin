74267e53ab6071d3a46debe4c40bb992b45c0975
刘海威 <mizonerom@hotmail.com>
2024-02-05 10:59:05 +0800

    ✨ feat: 格式化模板

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-
-打包插件
+# 环境安装
 npm install -g vsce
+
+# 初始化
+npm i
+
+# 打包插件
 vsce package
--- src/template/index.ts
+++ src/template/index.ts
@@ -1,22 +1,20 @@
 const getVueFile = (fileName: string) => {
-  const fileContent = `
-  <template>
-   ${fileName}
-  </template>
+  const fileContent = `<template>
+  ${fileName}
+</template>
+<script lang="ts">
+import { defineView } from '@kmsoft/upf-core'
+import { ${fileName}PropOptions } from './interface'
+import ${fileName}ViewModel from './${fileName}ViewModel'
 
-  <script lang="ts">
-  import { defineView } from '@kmsoft/upf-core'
-  import { ${fileName}PropOptions } from './interface'
-  import ${fileName}ViewModel from './${fileName}ViewModel'
-  
-  export default defineView({
-    name: '${fileName}',
-    props: ${fileName}PropOptions,
-    viewModel: ${fileName}ViewModel,
-    data() {}
-  })
-  </script>  
-  `;
+export default defineView({
+  name: '${fileName}',
+  props: ${fileName}PropOptions,
+  viewModel: ${fileName}ViewModel,
+  data() {}
+})
+</script>
+`;
 
   return {
     fileName: `${fileName}.vue`,
@@ -25,16 +23,15 @@ const getVueFile = (fileName: string) => {
 };
 
 const getVueModelFile = (fileName: string) => {
-  const fileContent = `
-  import { BaseViewModel } from '@kmsoft/upf-core'
-  import { I${fileName}EventEmits, I${fileName}State, ${fileName}PropType } from './interface'
-  
-  /** 导航管理 */
-  export default class ${fileName}ViewModel extends BaseViewModel<I${fileName}State, I${fileName}EventEmits, ${fileName}PropType> {
-    /** 加载完成函数 */
-    viewDidMount() {}
-  }
-  `;
+  const fileContent = `import { BaseViewModel } from '@kmsoft/upf-core'
+import { I${fileName}EventEmits, I${fileName}State, ${fileName}PropType } from './interface'
+
+/** 导航管理 */
+export default class ${fileName}ViewModel extends BaseViewModel<I${fileName}State, I${fileName}EventEmits, ${fileName}PropType> {
+  /** 加载完成函数 */
+  viewDidMount() {}
+}
+`;
 
   return {
     fileName: `${fileName}ViewModel.ts`,
@@ -43,23 +40,22 @@ const getVueModelFile = (fileName: string) => {
 };
 
 const getInterfaceFile = (fileName: string) => {
-  const fileContent = `
-  import { IBaseViewEventEmits, BaseViewOptions, ViewPropsTypeExtract, VuePropTypes } from '@kmsoft/upf-core'
+  const fileContent = `import { IBaseViewEventEmits, BaseViewOptions, ViewPropsTypeExtract, VuePropTypes } from '@kmsoft/upf-core'
 
-  /** ${fileName} 状态 **/
-  export interface I${fileName}State {}
-  
-  /** ${fileName} 事件 **/
-  export interface I${fileName}EventEmits extends IBaseViewEventEmits {}
-  
-  /** ${fileName} 参数 **/
-  export const ${fileName}PropOptions = {
-    ...BaseViewOptions
-  }
-  
-  /** ${fileName} 参数类型 **/
-  export type ${fileName}PropType = ViewPropsTypeExtract<typeof ${fileName}PropOptions>  
-  `;
+/** ${fileName} 状态 **/
+export interface I${fileName}State {}
+
+/** ${fileName} 事件 **/
+export interface I${fileName}EventEmits extends IBaseViewEventEmits {}
+
+/** ${fileName} 参数 **/
+export const ${fileName}PropOptions = {
+  ...BaseViewOptions
+}
+
+/** ${fileName} 参数类型 **/
+export type ${fileName}PropType = ViewPropsTypeExtract<typeof ${fileName}PropOptions>
+`;
 
   return {
     fileName: `interface.ts`,
@@ -68,13 +64,12 @@ const getInterfaceFile = (fileName: string) => {
 };
 
 const getIndexFile = (fileName: string) => {
-  const fileContent = `
-  import { connect } from '@kmsoft/upf-core'
-  import ${fileName} from './${fileName}.vue'
-  import ${fileName}ViewModel from './${fileName}ViewModel'
-  
-  export default connect(${fileName}, ${fileName}ViewModel)  
-  `;
+  const fileContent = `import { connect } from '@kmsoft/upf-core'
+import ${fileName} from './${fileName}.vue'
+import ${fileName}ViewModel from './${fileName}ViewModel'
+
+export default connect(${fileName}, ${fileName}ViewModel)
+`;
 
   return {
     fileName: `index.ts`,
