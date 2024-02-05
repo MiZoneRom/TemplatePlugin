863230588157a61795a870627b89d8d77d4f7375
刘海威 <mizonerom@hotmail.com>
2024-02-05 09:47:16 +0800

    ✨ feat: 完善模板

--- src/extension.ts
+++ src/extension.ts
@@ -1,54 +1,74 @@
+import fs from "fs";
+import { default as Templates } from "./template";
 
-const fs = require('fs');
-import { default as Templates } from './template';
-
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "template.create-template",
+    (args) => {
+      useExtension(args);
+    }
+  );
 
-	let disposable = vscode.commands.registerCommand('template.create-template', (args) => {
-		useExtension(args);
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-export function deactivate() { }
+export function deactivate() {}
 
 const useExtension = async (args: any) => {
-	try {
-		const path = vscode.workspace.workspaceFolders;
+  try {
+    const path = vscode.workspace.workspaceFolders;
 
-		if (!path || path.length <= 0) {
-			vscode.window.showErrorMessage('请先打开一个工作区!');
-			return;
-		}
+    if (!path || path.length <= 0) {
+      vscode.window.showErrorMessage("请先打开一个工作区!");
+      return;
+    }
 
-		const text = await vscode.window.showInputBox({ title: '请输入文件名称', prompt: '文件名将会作为组件名称创建' });
+    const fileName = await vscode.window.showInputBox({
+      title: "请输入组件名称",
+      prompt: "输入的名称将会作为组件名称创建",
+    });
 
-		if (!text) {
-			return;
-		}
+    if (!fileName) {
+      return;
+    }
 
-		// 文件夹路径
-		const selectPath = args.fsPath;
+    // 文件夹路径
+    const selectPath = args.fsPath;
 
-		for (const iterator of Templates) {
-			// 获取结果
-			const result = iterator(text);
-			// 创建路径
-			const indexjsPath = `${selectPath}/${result.fileName}`;
+    /** 文件夹名称 */
+    const folderName = toDashCase(fileName);
+    /** 目标文件夹路径 */
+    const folderPath = `${selectPath}/${folderName}`;
 
-			if (!fs.existsSync(indexjsPath)) {
-				fs.writeFileSync(indexjsPath, result.fileContent);
-			} else {
-				vscode.window.showErrorMessage('该目录下已存在 index.vue 文件!');
-				return;
-			}
-		}
+    if (fs.existsSync(folderPath)) {
+      vscode.window.showErrorMessage(`文件夹 ${folderName} 已存在`);
+    }
 
-	}
-	catch (error) {
-		vscode.window.showErrorMessage(JSON.stringify(error));
-	}
+    fs.mkdirSync(folderPath);
+
+    for (const iterator of Templates) {
+      // 获取结果
+      const result = iterator(fileName);
+      // 创建路径
+      const indexjsPath = `${folderPath}/${result.fileName}`;
+
+      if (!fs.existsSync(indexjsPath)) {
+        fs.writeFileSync(indexjsPath, result.fileContent);
+      } else {
+        vscode.window.showErrorMessage("该目录下已存在 index.vue 文件!");
+        return;
+      }
+    }
+  } catch (error) {
+    vscode.window.showErrorMessage(JSON.stringify(error));
+  }
+};
+
+const toDashCase = (str: string) => {
+  return str
+    .replace(/([A-Z])/g, "-$1")
+    .toLowerCase()
+    .replace(/^-+|-+$/g, "");
 };
--- src/template/index.ts
+++ src/template/index.ts
@@ -1,51 +1,64 @@
-
-
 const getVueFile = (fileName: string) => {
-
   const fileContent = `
   <template>
-    <div></div>
+   ${fileName}
   </template>
 
-  <script setup>
-  import { onMounted } from 'vue'
-
-  const handleData = () => {
-    console.log('handle data!')
-  }
-
-  onMounted(() => {
-    handleData()
+  <script lang="ts">
+  import { defineView } from '@kmsoft/upf-core'
+  import { ${fileName}PropOptions } from './interface'
+  import ${fileName}ViewModel from './${fileName}ViewModel'
+  
+  export default defineView({
+    name: '${fileName}',
+    props: ${fileName}PropOptions,
+    viewModel: ${fileName}ViewModel,
+    data() {}
   })
-
-  </script>
-
-  <style scoped>
-  </style>
+  </script>  
   `;
 
   return {
     fileName: `${fileName}.vue`,
-    fileContent: fileContent
+    fileContent: fileContent,
   };
 };
 
 const getVueModelFile = (fileName: string) => {
-
   const fileContent = `
-  export class Test {}
+  import { BaseViewModel } from '@kmsoft/upf-core'
+  import { I${fileName}EventEmits, I${fileName}State, ${fileName}PropType } from './interface'
+  
+  /** 导航管理 */
+  export default class ${fileName}ViewModel extends BaseViewModel<I${fileName}State, I${fileName}EventEmits, ${fileName}PropType> {
+    /** 加载完成函数 */
+    viewDidMount() {}
+  }
   `;
 
   return {
     fileName: `${fileName}ViewModel.ts`,
-    fileContent: fileContent
+    fileContent: fileContent,
   };
 };
 
 const getInterfaceFile = (fileName: string) => {
-
   const fileContent = `
-  export class Test {}
+  import { IBaseViewEventEmits, BaseViewOptions, ViewPropsTypeExtract, VuePropTypes } from '@kmsoft/upf-core'
+
+  /** ${fileName} 状态 **/
+  export interface I${fileName}State {}
+  
+  /** ${fileName} 事件 **/
+  export interface I${fileName}EventEmits extends IBaseViewEventEmits {}
+  
+  /** ${fileName} 参数 **/
+  export const ${fileName}PropOptions = {
+    ...BaseViewOptions
+  }
+  
+  /** ${fileName} 参数类型 **/
+  export type ${fileName}PropType = ViewPropsTypeExtract<typeof ${fileName}PropOptions>  
   `;
 
   return {
@@ -55,9 +68,12 @@ const getInterfaceFile = (fileName: string) => {
 };
 
 const getIndexFile = (fileName: string) => {
-
   const fileContent = `
-  export class Test {}
+  import { connect } from '@kmsoft/upf-core'
+  import ${fileName} from './${fileName}.vue'
+  import ${fileName}ViewModel from './${fileName}ViewModel'
+  
+  export default connect(${fileName}, ${fileName}ViewModel)  
   `;
 
   return {
@@ -66,11 +82,6 @@ const getIndexFile = (fileName: string) => {
   };
 };
 
-const templates = [
-  getVueFile,
-  getVueModelFile,
-  getInterfaceFile,
-  getIndexFile
-];
+const templates = [getVueFile, getVueModelFile, getInterfaceFile, getIndexFile];
 
 export default templates;
