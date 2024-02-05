716217df73b36a84f07fcafab2f5d975568cfac2
刘海威 <mizonerom@hotmail.com>
2024-02-05 10:14:41 +0800

    ✨ feat: 新增公共组件提示

--- src/extension.ts
+++ src/extension.ts
@@ -17,30 +17,56 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {}
 
 const useExtension = async (args: any) => {
-  try {
-    const path = vscode.workspace.workspaceFolders;
+  const path = vscode.workspace.workspaceFolders;
 
-    if (!path || path.length <= 0) {
-      vscode.window.showErrorMessage("请先打开一个工作区!");
+  if (!path || path.length <= 0) {
+    vscode.window.showErrorMessage("请先打开一个工作区!");
+    return;
+  }
+
+  const fileName = await vscode.window.showInputBox({
+    title: "请输入组件名称",
+    prompt: "输入的名称将会作为组件名称创建",
+  });
+
+  if (!fileName) {
+    return;
+  }
+
+  const quickPick = vscode.window.createQuickPick();
+  quickPick.title = "是否是公共组件";
+  quickPick.items = [
+    {
+      label: "是",
+    },
+    {
+      label: "否",
+    },
+  ];
+
+  quickPick.onDidChangeSelection((e) => {
+    if (e.length <= 0) {
       return;
     }
-
-    const fileName = await vscode.window.showInputBox({
-      title: "请输入组件名称",
-      prompt: "输入的名称将会作为组件名称创建",
-    });
-
-    if (!fileName) {
-      return;
-    }
-
     // 文件夹路径
     const selectPath = args.fsPath;
+    const selName = e[0].label;
+    createFiles(selectPath, fileName, selName == "是");
+    quickPick.hide();
+  });
+  quickPick.show();
+};
 
+const createFiles = (
+  path: string,
+  fileName: string,
+  publicControl: boolean
+) => {
+  try {
     /** 文件夹名称 */
     const folderName = toDashCase(fileName);
     /** 目标文件夹路径 */
-    const folderPath = `${selectPath}/${folderName}`;
+    const folderPath = `${path}/${folderName}`;
 
     if (fs.existsSync(folderPath)) {
       vscode.window.showErrorMessage(`文件夹 ${folderName} 已存在`);
@@ -49,8 +75,9 @@ const useExtension = async (args: any) => {
     fs.mkdirSync(folderPath);
 
     for (const iterator of Templates) {
+      const preFix = publicControl ? "K" : "";
       // 获取结果
-      const result = iterator(fileName);
+      const result = iterator(preFix + fileName);
       // 创建路径
       const indexjsPath = `${folderPath}/${result.fileName}`;
 
