8ed6317a4320d1a7c87e8553514259579efd5ecd
liuhw <liuhw@kmsoft.com.cn>
2024-02-25 19:13:33 +0800

    ✨ feat: 新增模板配置功能

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,8 @@
 import fs from "fs";
-import { default as Templates } from "./template";
+import { default as Template } from "./template";
 
 import * as vscode from "vscode";
+import { FileTemplate } from "./types";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
@@ -24,43 +25,37 @@ const useExtension = async (args: any) => {
     return;
   }
 
-  const fileName = await vscode.window.showInputBox({
-    title: "请输入组件名称",
-    prompt: "输入的名称将会作为组件名称创建",
-  });
+  // 文件夹路径
+  const selectPath = args.fsPath;
+
+  /** 获取模板类型 */
+  const template = await getTemplate();
+
+  if (!template) {
+    return;
+  }
+
+  /** 文件名 */
+  const fileName = await getFileName();
 
   if (!fileName) {
     return;
   }
 
-  const quickPick = vscode.window.createQuickPick();
-  quickPick.title = "是否是公共组件";
-  quickPick.items = [
-    {
-      label: "是",
-    },
-    {
-      label: "否",
-    },
-  ];
+  const isPublic = await getIsPublicView();
 
-  quickPick.onDidChangeSelection((e) => {
-    if (e.length <= 0) {
-      return;
-    }
-    // 文件夹路径
-    const selectPath = args.fsPath;
-    const selName = e[0].label;
-    createFiles(selectPath, fileName, selName == "是");
-    quickPick.hide();
-  });
-  quickPick.show();
+  if (isPublic == undefined) {
+    return;
+  }
+
+  createFiles(selectPath, fileName, isPublic, template);
 };
 
 const createFiles = (
   path: string,
   fileName: string,
-  publicControl: boolean
+  publicControl: boolean,
+  template: FileTemplate
 ) => {
   try {
     /** 文件夹名称 */
@@ -74,15 +69,20 @@ const createFiles = (
 
     fs.mkdirSync(folderPath);
 
-    for (const iterator of Templates) {
+    for (const iterator of template.files) {
       const preFix = publicControl ? "K" : "";
-      // 获取结果
-      const result = iterator(preFix + fileName);
+      const finalFileName = preFix + fileName;
       // 创建路径
-      const indexjsPath = `${folderPath}/${result.fileName}`;
+      const indexjsPath = `${folderPath}/${iterator.fileName.replaceAll(
+        "{{fileName}}",
+        finalFileName
+      )}`;
 
       if (!fs.existsSync(indexjsPath)) {
-        fs.writeFileSync(indexjsPath, result.fileContent);
+        fs.writeFileSync(
+          indexjsPath,
+          iterator.fileContent.replaceAll("{{fileName}}", finalFileName)
+        );
       } else {
         vscode.window.showErrorMessage("该目录下已存在 index.vue 文件!");
         return;
@@ -98,4 +98,93 @@ const toDashCase = (str: string) => {
     .replace(/([A-Z])/g, "-$1")
     .toLowerCase()
     .replace(/^-+|-+$/g, "");
+};
+
+/** 获取所有模板 */
+const getTemplates = () => {
+  const customCommitType = vscode.workspace
+    .getConfiguration("Files")
+    .get<Array<FileTemplate>>("Templates");
+
+  if (!customCommitType || customCommitType.length <= 0) {
+    return [Template];
+  }
+
+  return customCommitType;
+};
+
+/** 获取文件名 */
+const getFileName = async () => {
+  const fileName = await vscode.window.showInputBox({
+    title: "请输入组件名称",
+    prompt: "输入的名称将会作为组件名称创建",
+  });
+
+  if (!fileName) {
+    return;
+  }
+
+  return fileName;
+};
+
+/**
+ * 选择模板
+ */
+const getTemplate = () => {
+  return new Promise<FileTemplate | undefined>((reslove, reject) => {
+    const templates = getTemplates();
+
+    console.info(templates);
+
+    const quickPick = vscode.window.createQuickPick();
+    quickPick.title = "请选择模板";
+    quickPick.items = templates.map((a) => {
+      return { label: a.templateName };
+    });
+
+    quickPick.onDidChangeSelection((e) => {
+      if (e.length <= 0) {
+        reslove(undefined);
+        return;
+      }
+
+      const selName = e[0].label;
+
+      reslove(templates.find((a) => a.templateName == selName));
+
+      quickPick.hide();
+    });
+    quickPick.show();
+  });
+};
+
+/** 获取是否公用组件 */
+const getIsPublicView = () => {
+  return new Promise<boolean | undefined>((reslove, reject) => {
+    const quickPick = vscode.window.createQuickPick();
+    quickPick.title = "是否是公共组件";
+    quickPick.items = [
+      {
+        label: "是",
+      },
+      {
+        label: "否",
+      },
+    ];
+
+    quickPick.onDidChangeSelection((e) => {
+      if (e.length <= 0) {
+        reslove(undefined);
+        return;
+      }
+
+      const selName = e[0].label;
+
+      reslove(selName == "是");
+
+      quickPick.hide();
+    });
+
+    quickPick.show();
+  });
 };
--- src/template/index.ts
+++ src/template/index.ts
@@ -1,82 +1,71 @@
-const getVueFile = (fileName: string) => {
-  const fileContent = `<template>
-  ${fileName}
+import { FileTemplate } from "../types";
+
+const vueFile = {
+  fileName: `{{fileName}}.vue`,
+  fileContent: `<template>
+  {{fileName}}
 </template>
 <script lang="ts">
 import { defineView } from '@kmsoft/upf-core'
-import { ${fileName}PropOptions } from './interface'
-import ${fileName}ViewModel from './${fileName}ViewModel'
+import { {{fileName}}PropOptions } from './interface'
+import {{fileName}}ViewModel from './{{fileName}}ViewModel'
 
 export default defineView({
-  name: '${fileName}',
-  props: ${fileName}PropOptions,
-  viewModel: ${fileName}ViewModel,
+  name: '{{fileName}}',
+  props: {{fileName}}PropOptions,
+  viewModel: {{fileName}}ViewModel,
   data() {}
 })
 </script>
-`;
-
-  return {
-    fileName: `${fileName}.vue`,
-    fileContent: fileContent,
-  };
+`,
 };
 
-const getVueModelFile = (fileName: string) => {
-  const fileContent = `import { BaseViewModel } from '@kmsoft/upf-core'
-import { I${fileName}EventEmits, I${fileName}State, ${fileName}PropType } from './interface'
-
-/** 导航管理 */
-export default class ${fileName}ViewModel extends BaseViewModel<I${fileName}State, I${fileName}EventEmits, ${fileName}PropType> {
-  /** 加载完成函数 */
-  viewDidMount() {}
-}
-`;
-
-  return {
-    fileName: `${fileName}ViewModel.ts`,
-    fileContent: fileContent,
-  };
+const vueModelFile = {
+  fileName: `{{fileName}}ViewModel.ts`,
+  fileContent: `import { BaseViewModel } from '@kmsoft/upf-core'
+  import { I{{fileName}}EventEmits, I{{fileName}}State, {{fileName}}PropType } from './interface'
+  
+  /** 导航管理 */
+  export default class {{fileName}}ViewModel extends BaseViewModel<I{{fileName}}State, I{{fileName}}EventEmits, {{fileName}}PropType> {
+    /** 加载完成函数 */
+    viewDidMount() {}
+  }
+  `,
 };
 
-const getInterfaceFile = (fileName: string) => {
-  const fileContent = `import { IBaseViewEventEmits, BaseViewOptions, ViewPropsTypeExtract, VuePropTypes } from '@kmsoft/upf-core'
+const interfaceFile = {
+  fileName: `interface.ts`,
+  fileContent: `import { IBaseViewEventEmits, BaseViewOptions, ViewPropsTypeExtract, VuePropTypes } from '@kmsoft/upf-core'
 
-/** ${fileName} 状态 **/
-export interface I${fileName}State {}
+/** {{fileName}} 状态 **/
+export interface I{{fileName}}State {}
 
-/** ${fileName} 事件 **/
-export interface I${fileName}EventEmits extends IBaseViewEventEmits {}
+/** {{fileName}} 事件 **/
+export interface I{{fileName}}EventEmits extends IBaseViewEventEmits {}
 
-/** ${fileName} 参数 **/
-export const ${fileName}PropOptions = {
+/** {{fileName}} 参数 **/
+export const {{fileName}}PropOptions = {
   ...BaseViewOptions
 }
 
-/** ${fileName} 参数类型 **/
-export type ${fileName}PropType = ViewPropsTypeExtract<typeof ${fileName}PropOptions>
-`;
-
-  return {
-    fileName: `interface.ts`,
-    fileContent: fileContent,
-  };
+/** {{fileName}} 参数类型 **/
+export type {{fileName}}PropType = ViewPropsTypeExtract<typeof {{fileName}}PropOptions>
+`,
 };
 
-const getIndexFile = (fileName: string) => {
-  const fileContent = `import { connect } from '@kmsoft/upf-core'
-import ${fileName} from './${fileName}.vue'
-import ${fileName}ViewModel from './${fileName}ViewModel'
+const indexFile = {
+  fileName: `index.ts`,
+  fileContent: `import { connect } from '@kmsoft/upf-core'
+import {{fileName}} from './{{fileName}}.vue'
+import {{fileName}}ViewModel from './{{fileName}}ViewModel'
 
-export default connect(${fileName}, ${fileName}ViewModel)
-`;
-
-  return {
-    fileName: `index.ts`,
-    fileContent: fileContent,
-  };
+export default connect({{fileName}}, {{fileName}}ViewModel)
+`,
 };
 
-const templates = [getVueFile, getVueModelFile, getInterfaceFile, getIndexFile];
+const template: FileTemplate = {
+  templateName: "默认模板",
+  files: [vueFile, vueModelFile, interfaceFile, indexFile],
+};
 
-export default templates;
+export default template;
